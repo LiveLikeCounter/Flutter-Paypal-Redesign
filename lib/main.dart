6675d2ee9f8f7c2cd2f864202bfff925d17435ca
Johan Walhout <johanwalhout@gmail.com>
2019-05-27 21:35:35 +0200

    Header and card setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,149 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_paypalredesign/util.dart';
+
 void main() => runApp(PaypalApp());
 
 class PaypalApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: SafeArea(
-        child: Scaffold(
-          appBar: AppBar(
-            title: Text('Welcome to Flutter'),
-          ),
-          body: Center(
-            child: Text('Hello World'),
-          ),
-        ),
-      ),
+      // color: Colors.white,
+      debugShowCheckedModeBanner: false,
+      home: HomeApp(),
     );
   }
+}
+
+class HomeApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      children: <Widget>[
+        Scaffold(
+          backgroundColor: Colors.white,
+          appBar: mainAppBar(),
+          body: Column(
+            children: <Widget>[
+              Card(
+                child: Container(
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage('assets/images/Paypal-logo.png'),
+                      fit: BoxFit.none,
+                      alignment: Alignment.centerRight,
+                    ),
+                  ),
+                  child: Column(
+                    children: <Widget>[
+                      Row(
+                        children: <Widget>[
+                          Image.asset('assets/images/Paypal-logo.png'),
+                          Text('BALANCE'),
+                          Icon(Icons.info_outline)
+                        ],
+                      ),
+                      Row(
+                        children: <Widget>[
+                          Image.asset('assets/images/chip_thumb.png'),
+                          Column(
+                            children: <Widget>[
+                              Text('\$452,20'),
+                              Text('Available')
+                            ],
+                          )
+                        ],
+                      ),
+                      Row(
+                        children: <Widget>[
+                          FlatButton(
+                            child: Text('MR.RAGNAR LOTHBROK'),
+                            onPressed: () {
+                              print('object');
+                            },
+                          ),
+                          Spacer()
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.all(15),
+                elevation: 6,
+                child: Column(
+                  children: <Widget>[
+                    Row(
+                      children: <Widget>[
+                        Image.asset('assets/images/Paypal-logo.png'),
+                        Text('BALANCE'),
+                        Icon(Icons.info_outline)
+                      ],
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Image.asset('assets/images/chip_thumb.png'),
+                        Column(
+                          children: <Widget>[
+                            Text('\$452,20'),
+                            Text('Available')
+                          ],
+                        )
+                      ],
+                    ),
+                    Row(
+                      children: <Widget>[
+                        FlatButton(
+                          child: Text('MR.RAGNAR LOTHBROK'),
+                          onPressed: () {
+                            print('object');
+                          },
+                        ),
+                        Spacer()
+                      ],
+                    )
+                  ],
+                ),
+              ),
+              Text('Activity'),
+              ListView(
+                shrinkWrap: true,
+                children: <Widget>[
+                  ListTile(
+                    leading: Icon(Icons.map),
+                    title: Text('Map'),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.photo_album),
+                    title: Text('Album'),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.phone),
+                    title: Text('Phone'),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+mainAppBar() {
+  return AppBar(
+    leading: Image.asset(
+      'assets/images/icon_settings.png',
+      color: PaypalColors.DarkBlue,
+    ),
+    title: Image.asset('assets/images/Paypal-logo-header.png'),
+    actions: <Widget>[
+      Image.asset('assets/images/icon_school-bell.png',
+          color: PaypalColors.DarkBlue)
+    ],
+    backgroundColor: Colors.transparent,
+    elevation: 0.0,
+  );
 }
--- lib/util.dart
+++ lib/util.dart
@@ -7,8 +7,8 @@ double screenAwareSize(double size, BuildContext context) {
 }
 
 class PaypalColors {
-  static const LightBlue = Color.fromRGBO(0, 154, 224, 0);
-  static const DarkBlue = Color.fromRGBO(18, 106, 175, 0);
-  static const Grey = Color.fromRGBO(157, 157, 157, 0);
+  static const LightBlue = Color.fromRGBO(0, 154, 224, 1);
+  static const DarkBlue = Color.fromRGBO(18, 106, 175, 1);
+  static const Grey = Color.fromRGBO(157, 157, 157, 1);
 }
 
