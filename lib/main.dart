88f694b491d373f03133e9fdcc0da2d3bb4d51fc
Johan Walhout <johanwalhout@gmail.com>
2019-05-28 21:31:39 +0200

    Card content

--- lib/main.dart
+++ lib/main.dart
@@ -25,84 +25,79 @@ class HomeApp extends StatelessWidget {
           appBar: mainAppBar(),
           body: Column(
             children: <Widget>[
-              Card(
-                child: Container(
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      image: AssetImage('assets/images/Paypal-logo.png'),
-                      fit: BoxFit.none,
-                      alignment: Alignment.centerRight,
-                    ),
-                  ),
-                  child: Column(
-                    children: <Widget>[
-                      Row(
-                        children: <Widget>[
-                          Image.asset('assets/images/Paypal-logo.png'),
-                          Text('BALANCE'),
-                          Icon(Icons.info_outline)
-                        ],
-                      ),
-                      Row(
-                        children: <Widget>[
-                          Image.asset('assets/images/chip_thumb.png'),
-                          Column(
-                            children: <Widget>[
-                              Text('\$452,20'),
-                              Text('Available')
-                            ],
-                          )
-                        ],
-                      ),
-                      Row(
-                        children: <Widget>[
-                          FlatButton(
-                            child: Text('MR.RAGNAR LOTHBROK'),
-                            onPressed: () {
-                              print('object');
-                            },
-                          ),
-                          Spacer()
-                        ],
-                      )
-                    ],
-                  ),
-                ),
-              ),
-              Card(
+              Container(
                 margin: EdgeInsets.all(15),
-                elevation: 6,
+                padding: EdgeInsets.symmetric(horizontal: 16, vertical: 11),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(
+                      color: Colors.white, width: 0, style: BorderStyle.solid),
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(5.0),
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                        color: Colors.grey,
+                        offset: Offset(0, 3),
+                        blurRadius: 6,
+                        spreadRadius: 1)
+                  ],
+                ),
                 child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: <Widget>[
                     Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: <Widget>[
-                        Image.asset('assets/images/Paypal-logo.png'),
-                        Text('BALANCE'),
+                        Row(
+                          children: <Widget>[
+                            Image.asset('assets/images/Paypal-logo.png',
+                                height: 30),
+                            SizedBox(width: 5),
+                            Text('BALANCE',
+                                style: TextStyle(color: PaypalColors.DarkBlue)),
+                          ],
+                        ),
                         Icon(Icons.info_outline)
                       ],
                     ),
+                    SizedBox(height: 20),
                     Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: <Widget>[
                         Image.asset('assets/images/chip_thumb.png'),
                         Column(
+                          crossAxisAlignment: CrossAxisAlignment.end,
                           children: <Widget>[
-                            Text('\$452,20'),
-                            Text('Available')
+                            Row(
+                              children: <Widget>[
+                                Text('\$', style: TextStyle(fontSize: 25)),
+                                Text('452,20', style: TextStyle(fontSize: 40)),
+                              ],
+                            ),
+                            Text('Available', style: TextStyle(color: PaypalColors.Grey)),
                           ],
                         )
                       ],
                     ),
+                    SizedBox(height: 20),
                     Row(
                       children: <Widget>[
-                        FlatButton(
-                          child: Text('MR.RAGNAR LOTHBROK'),
-                          onPressed: () {
-                            print('object');
-                          },
+                        SizedBox(
+                          height: 25,
+                          child: FlatButton(
+                              color: PaypalColors.LightGrey,
+                              textColor: PaypalColors.DarkBlue,
+                              child: Text("MR.RAGNAR LOTHBROK"),
+                              onPressed: () {
+                                print('A');
+                              },
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(30.0))),
                         ),
                         Spacer()
                       ],
-                    )
+                    ),
                   ],
                 ),
               ),
@@ -138,7 +133,7 @@ mainAppBar() {
       'assets/images/icon_settings.png',
       color: PaypalColors.DarkBlue,
     ),
-    title: Image.asset('assets/images/Paypal-logo-header.png'),
+    title: Image.asset('assets/images/Paypal-logo-header.png', height: 25),
     actions: <Widget>[
       Image.asset('assets/images/icon_school-bell.png',
           color: PaypalColors.DarkBlue)
--- lib/util.dart
+++ lib/util.dart
@@ -9,6 +9,7 @@ double screenAwareSize(double size, BuildContext context) {
 class PaypalColors {
   static const LightBlue = Color.fromRGBO(0, 154, 224, 1);
   static const DarkBlue = Color.fromRGBO(18, 106, 175, 1);
+  static const LightGrey = Color.fromRGBO(242, 242, 242, 1);
   static const Grey = Color.fromRGBO(157, 157, 157, 1);
 }
 
