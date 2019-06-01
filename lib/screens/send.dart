f0b507af567ef17ba0c259fc7c71c8671b524566
Johan Walhout <johanwalhout@gmail.com>
2019-06-01 17:36:21 +0200

    Send1 page added

--- lib/screens/send.dart
+++ lib/screens/send.dart
@@ -1,4 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+import 'package:flutter_paypalredesign/util.dart';
+import 'package:flutter_paypalredesign/screens/send1.dart';
 
 class Send extends StatefulWidget {
   @override
@@ -9,7 +13,142 @@ class _SendState extends State<Send> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.red,
+      child: Column(
+        children: <Widget>[
+          Container(
+            width: MediaQuery.of(context).size.width * 0.9,
+            height: 40,
+            child: Theme(
+              data: ThemeData(
+                primaryColor: PaypalColors.LightGrey,
+                primaryColorDark: PaypalColors.LightGrey,
+              ),
+              child: TextFormField(
+                textAlign: TextAlign.left,
+                style: TextStyle(fontSize: 16),
+                decoration: InputDecoration(
+                    contentPadding: EdgeInsets.all(5),
+                    fillColor: PaypalColors.LightGrey,
+                    filled: true,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15.0),
+                      borderSide:
+                          BorderSide(color: PaypalColors.LightGrey, width: 0.0),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15.0),
+                      borderSide:
+                          BorderSide(color: PaypalColors.LightGrey, width: 0.0),
+                    ),
+                    prefixIcon: Padding(
+                      padding: EdgeInsets.all(0.0),
+                      child: Icon(
+                        Icons.search,
+                        color: PaypalColors.Grey,
+                      ),
+                    ),
+                    hintText: "Search name, email, phone number"),
+              ),
+            ),
+          ),
+          SizedBox(height: 15),
+          Divider(
+            height: 0.1,
+            color: Color.fromRGBO(0, 0, 0, 0.3),
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(vertical: 25),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.only(left: 20),
+                  child: Text(
+                    'Suggestions',
+                    style: TextStyle(
+                        fontFamily: "worksans",
+                        fontSize: 16,
+                        color: PaypalColors.Grey),
+                  ),
+                ),
+                SizedBox(height: 5),
+                ListView(
+                  shrinkWrap: true,
+                  children: <Widget>[
+                    ListTile(
+                      leading: Container(
+                        width: 60,
+                        height: 60,
+                        child: CircleAvatar(
+                          backgroundColor: Colors.transparent,
+                          backgroundImage: AssetImage(
+                              'assets/images/if_3_avatar_2754579.png'),
+                        ),
+                      ),
+                      title: Text('Floki',
+                          style: TextStyle(fontFamily: "worksans")),
+                      subtitle: Text('paypal.me/manosthegods',
+                          style: TextStyle(fontFamily: "worksans")),
+                      onTap: () {
+                        print('Tabbed');
+                        Navigator.of(context).push(
+                          MaterialPageRoute(builder: (context) => Send1()),
+                        );
+                      },
+                    ),
+                    ListTile(
+                      leading: Container(
+                        width: 60,
+                        height: 60,
+                        child: CircleAvatar(
+                          backgroundColor: Colors.transparent,
+                          backgroundImage: AssetImage(
+                              'assets/images/if_9_avatar_2754584.png'),
+                        ),
+                      ),
+                      title: Text('Lagertha Lothbrok',
+                          style: TextStyle(fontFamily: "worksans")),
+                      subtitle: Text('paypal.me/queenofKat',
+                          style: TextStyle(fontFamily: "worksans")),
+                    ),
+                    ListTile(
+                      leading: Container(
+                        width: 60,
+                        height: 60,
+                        child: CircleAvatar(
+                          backgroundColor: Colors.transparent,
+                          backgroundImage: AssetImage(
+                              'assets/images/if_12_avatar_2754577.png'),
+                        ),
+                      ),
+                      title: Text('Bjorn Lothbrok',
+                          style: TextStyle(fontFamily: "worksans")),
+                      subtitle: Text('paypal.me/bjornironside',
+                          style: TextStyle(fontFamily: "worksans")),
+                    ),
+                    ListTile(
+                      leading: Container(
+                        width: 60,
+                        height: 60,
+                        child: CircleAvatar(
+                          backgroundColor: Colors.transparent,
+                          backgroundImage: AssetImage(
+                              'assets/images/if_8_avatar_2754583.png'),
+                        ),
+                      ),
+                      title: Text('Ivar Lothbrok',
+                          style: TextStyle(fontFamily: "worksans")),
+                      subtitle: Text('paypal.me/theboneless',
+                          style: TextStyle(fontFamily: "worksans")),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
--- lib/screens/send1.dart
+++ lib/screens/send1.dart
@@ -1,3 +1,183 @@
 import 'package:flutter/material.dart';
 
 import 'package:flutter_paypalredesign/util.dart';
+
+class Send1 extends StatefulWidget {
+  @override
+  _Send1State createState() => _Send1State();
+}
+
+class _Send1State extends State<Send1> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          automaticallyImplyLeading: true,
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back, color: Colors.black,),
+            onPressed: () => Navigator.pop(context, false),
+          ),
+          title: Text('Send to Floki',
+              style: TextStyle(fontFamily: "worksans", color: Colors.black)),
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+        ),
+        body: SingleChildScrollView(
+          child: Container(
+            width: MediaQuery.of(context).size.width * 1,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  width: 150,
+                  height: 150,
+                  child: CircleAvatar(
+                    backgroundColor: Colors.transparent,
+                    backgroundImage:
+                        AssetImage('assets/images/if_3_avatar_2754579.png'),
+                  ),
+                ),
+                SizedBox(height: 15),
+                Text(
+                  'Floki',
+                  style: TextStyle(
+                      fontFamily: "worksans",
+                      fontSize: 22,
+                      fontWeight: FontWeight.w500),
+                ),
+                SizedBox(height: 15),
+                SizedBox(
+                  height: 25,
+                  child: FlatButton(
+                    color: PaypalColors.DarkBlue,
+                    textColor: Colors.white,
+                    child: Text(
+                      "paypal.me/manosthegods",
+                      style: TextStyle(
+                          fontFamily: "worksans",
+                          color: Colors.white,
+                          fontSize: 14,
+                          fontWeight: FontWeight.w300),
+                    ),
+                    onPressed: () {},
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30.0),
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.symmetric(horizontal: 20, vertical: 80),
+                  child: Column(
+                    children: <Widget>[
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Text(
+                            'Amount',
+                            style:
+                                TextStyle(fontFamily: "worksans", fontSize: 20),
+                          ),
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.end,
+                            children: <Widget>[
+                              Text(
+                                'USD',
+                                style: TextStyle(
+                                    fontFamily: "worksans",
+                                    fontSize: 12,
+                                    color: PaypalColors.Grey),
+                              ),
+                              SizedBox(width: 5),
+                              Text(
+                                '0,00',
+                                style: TextStyle(
+                                    fontFamily: "worksans",
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w500),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 10),
+                      Divider(height: 0.1, color: PaypalColors.Grey),
+                      SizedBox(height: 30),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Text(
+                            'Transaction fees',
+                            style:
+                                TextStyle(fontFamily: "worksans", fontSize: 20),
+                          ),
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.end,
+                            children: <Widget>[
+                              Text(
+                                'USD',
+                                style: TextStyle(
+                                    fontFamily: "worksans",
+                                    fontSize: 12,
+                                    color: PaypalColors.Grey),
+                              ),
+                              SizedBox(width: 5),
+                              Text(
+                                '0,00',
+                                style: TextStyle(
+                                    fontFamily: "worksans",
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w500),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 10),
+                      Divider(height: 0.1, color: PaypalColors.Grey),
+                      SizedBox(height: 30),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Image.asset('assets/images/Visa-logo.png'),
+                          Spacer(flex: 1),
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text(
+                                'Visa',
+                                style: TextStyle(
+                                    fontFamily: "worksans",
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w500),
+                              ),
+                              Text(
+                                'Debit ****3017',
+                                style: TextStyle(
+                                    fontFamily: "worksans",
+                                    fontSize: 12,
+                                    color: PaypalColors.Grey),
+                              ),
+                            ],
+                          ),
+                          Spacer(flex: 5),
+                          Icon(Icons.chevron_right,
+                              color: PaypalColors.Grey, size: 40),
+                        ],
+                      ),
+                      SizedBox(height: 10),
+                      Divider(height: 0.1, color: PaypalColors.Grey),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
